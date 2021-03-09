38845fa499a516b337dd1aab8d1d42c37f3f92d8
Spongebang <boy0501@naver.com>
2021-03-09 10:52:01 +0900

    찌꺼기 + 스언어 1주차과제

---
+++ 3-1스언어과제/1번과제.py
@@ -0,0 +1,56 @@
+print("#1번")
+width = 4.5
+height = 7.9
+rect_area = int(width * height*100)/100
+rect_perimeter = 2 * (width + height)
+print(rect_area,rect_perimeter)
+
+print("#2번")
+time = (45 * 60 + 30)/3600
+mile = 14 / 1.6
+mileperhour = int(mile/time*100)/100
+print(mileperhour)
+print("{0:.2f}",format(mileperhour))
+
+print("#3번")
+birth = 365*24*3600//7
+death = 365*24*3600//13
+migrate = 365*24*3600//45
+person = 312032486
+for a in range(5):
+    person = person+ birth - death + migrate
+    print(person)
+
+print("#4번")
+pound = eval(input("파운드값 입력:"))
+kilo = pound * 0.454
+print("{0:.2f} 파운드는 {1:.2f} 킬로그램".format(pound,kilo))
+
+print("#5번")
+subtotal,rate = eval(input("소계와 팁 비율 입력:"))
+tip = subtotal * rate / 100
+total = subtotal + tip
+print("팁은 {0:.2f} 총액은 {1:.2f}".format(tip,total))
+
+print("#6번")
+number = eval(input("0~1000사이 숫자입력:"))
+result = 0
+while number:
+    result = result + number % 10
+    number = number // 10
+print(result)
+
+print("#7번")
+min = eval(input("분에대한 숫자 입력:"))
+hour = min / 60
+day = hour / 24 % 365
+year = hour / 24 /365
+print("{0:.0f}년 {1:.0f}일 입니다".format(year,day))
+
+print("#8번")
+tempur = eval(input("화씨 -58f 와 41f 사이의 온도를 입 력하세요:"))
+wind = eval(input("풍속을 시간 당 마일 단위로 입력하세요:"))
+tem = 35.74 + 0.6215 * tempur - 35.75 * wind **0.16 + 0.4275 *tempur * wind **0.16
+print("{0:.5f} 는 체감온도입니다.".format(tem))
+
+
---
+++ termproject/start_state.py
@@ -0,0 +1,179 @@
+import gfw
+from pico2d import *
+import gobj
+import main_state
+import rank_state
+import random
+
+
+canvas_width = 640
+canvas_height = 480
+
+thunderVersion = [9,13,8]
+thunderType = 0
+
+def enter():
+    gfw.world.init([])
+
+    global bg,name,light,start,rank,mquit,select
+    bg = gfw.image.load('./res/bg/88.png')
+    name = gfw.image.load('./res/bg/97.png')
+    light = gfw.image.load('./res/bg/89.png')
+    start = gfw.image.load('./res/bg/Start.png')
+    rank = gfw.image.load('./res/bg/Rank.png')
+    mquit = gfw.image.load('./res/bg/Quit.png')
+    select = gfw.image.load('./res/bg/117.png')
+    global lightning,light_time
+    lightning = 0
+    light_time = 0
+    
+    global selecty,selected
+    selecty = 240
+    selected = 3
+    
+    global Smag,Rmag,Qmag
+    Smag = Rmag = Qmag = 1
+
+    global mfont
+    mfont = gfw.font.load('./res/ConsolaMalgun.ttf',15)
+
+    global bg_music,thunder_music
+    bg_music = load_wav('./res/미싱노브금/intro.wav')
+    bg_music.set_volume(60)
+    bg_music.repeat_play()
+    thunder_music = load_music('./res/미싱노브금/번개1.ogg')
+    thunder_music.set_volume(40)
+    thunder_music.play()
+    
+def lightfunc():
+    global light_time,lightning,thunder_music,thunderType,thunderVersion
+    light_time += gfw.delta_time
+    if light_time > random.uniform(3.0,6.0):
+        lightning = 160
+        light_time = 0
+        thunder_music.stop()
+        del thunder_music
+        thunderType = random.randint(0,2)
+        fmt = './res/미싱노브금/번개%d.ogg'        
+        fn = fmt % (thunderType + 1)
+        thunder_music = load_music(fn)
+        thunder_music.set_volume(40)
+        thunder_music.play()
+
+    if lightning- 3 >= 0:
+        lightning -= 3
+    SDL_SetTextureAlphaMod(light.texture,lightning)
+        
+def highlightButton():
+    global Smag,Rmag,Qmag
+    if selected == 3:
+        if Smag + 0.02 < 1.5:
+            Smag += 0.02
+        if Rmag - 0.05 > 1.0:
+            Rmag -= 0.05
+        if Qmag - 0.05 > 1.0:
+            Qmag -= 0.05
+    elif selected ==  2:
+        if Smag - 0.05 > 1.0:
+            Smag -= 0.05
+        if Rmag + 0.02 < 1.5:
+            Rmag += 0.02
+        if Qmag - 0.05 > 1.0:
+            Qmag -= 0.05
+    elif selected == 1:
+        if Smag - 0.05 > 1.0:
+            Smag -= 0.05
+        if Rmag - 0.05 > 1.0:
+            Rmag -= 0.05
+        if Qmag + 0.02 < 1.5:
+            Qmag += 0.02
+
+
+
+def update():
+    gfw.world.update()
+    lightfunc()
+    highlightButton()
+    check()
+    
+
+def check():
+    pass
+    
+
+def draw():
+    bg.draw(320,240)
+    light.draw(320,240)
+    name.draw(320,340)
+    if selected == 1:
+        start.draw(320,190,start.w*Smag,start.h*Smag)
+        rank.draw(320,120,rank.w*Rmag,rank.h*Rmag)
+        mquit.draw(320,50,mquit.w*Qmag,mquit.h*Qmag)
+    elif selected == 2:
+        start.draw(320,190,start.w*Smag,start.h*Smag)
+        mquit.draw(320,50,mquit.w*Qmag,mquit.h*Qmag)
+        rank.draw(320,120,rank.w*Rmag,rank.h*Rmag)
+    elif selected == 3:
+        rank.draw(320,120,rank.w*Rmag,rank.h*Rmag)
+        mquit.draw(320,50,mquit.w*Qmag,mquit.h*Qmag)
+        start.draw(320,190,start.w*Smag,start.h*Smag)
+
+
+    select.draw(320,selecty)
+    
+    
+    mfont.draw(30,canvas_height-10,"In game",(230,230,230))
+    mfont.draw(0,canvas_height-25,"(Move) Arrow keys",(230,230,230))
+    mfont.draw(0,canvas_height-40,"(jump) Left Shift",(230,230,230))
+    mfont.draw(0,canvas_height-55,"(attack) z",(230,230,230))
+    mfont.draw(canvas_width-100,canvas_height-10,"In menu",(230,230,230))
+    mfont.draw(canvas_width-155,canvas_height-25,"Arrow keys (Select)",(230,230,230))
+    mfont.draw(canvas_width-115,canvas_height-40,"Enter (Choose)",(230,230,230))
+    
+    gfw.world.draw()
+
+def pause():
+    global bg_music,thunder_music
+    thunder_music.stop()
+    del bg_music
+
+def resume():
+    gfw.world.init([])
+    global bg_music,thunder_music
+    bg_music = load_wav('./res/미싱노브금/intro.wav')
+    bg_music.set_volume(60)
+    bg_music.repeat_play()
+    thunder_music = load_music('./res/미싱노브금/번개1.ogg')
+    thunder_music.set_volume(40)
+    thunder_music.play()
+
+def handle_event(e):
+    global selecty,selected
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    elif e.type == SDL_KEYDOWN:
+        if e.key == SDLK_DOWN:
+            if selecty - 70 >30 :
+                selecty -= 70
+                selected -= 1
+        elif e.key == SDLK_UP:
+            if selecty + 70 <=240:
+                selecty += 70
+                selected += 1
+        elif e.key == SDLK_RETURN:
+            if selected == 3:
+                gfw.push(main_state)
+            elif selected == 2:
+                gfw.push(rank_state)
+            elif selected == 1:
+                gfw.quit()
+
+def exit():
+    global bg_music,thunder_music
+    #del bg_music
+    #thunder_music.stop()
+    del thunder_music
+
+if __name__ == '__main__':
+    gfw.run_main()
+
