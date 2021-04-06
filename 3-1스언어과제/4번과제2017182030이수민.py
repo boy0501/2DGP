37dc283712c804e80052d3cc1aff25ea27357369
Spongebang <boy0501@naver.com>
2021-04-06 10:56:57 +0900

    modify

--- 3-1스언어과제/4번과제2017182030이수민.py
+++ 3-1스언어과제/4번과제2017182030이수민.py
@@ -72,5 +72,78 @@ print("a의 속도:{0}반지름:{1}색상:{2}전원상태:{3}입니다".format(a
 print("b의 속도:{0}반지름:{1}색상:{2}전원상태:{3}입니다".format(b.getspd(),b.getrad(),b.getcolor(),b.geton()))
 
 #4번문제 7.8
+import time
 class StopWatch:
+    def __init__(self):
+        self.__startTime = time.time()
+        self.__endTime = time.time()
     
+    def start(self):
+        self.__startTime = time.time()
+    def end(self):
+        self.__endTime = time.time()
+    def getElapsedTime(self):
+        return int((self.__endTime - self.__startTime) * 1000 )
+
+d = StopWatch()
+a = 0
+for i in range(1000000):
+    a = a + i
+d.end()
+print("밀리초 : {0}".format(d.getElapsedTime()))
+
+#5번문제 12.1
+class Triangle:
+    def __init__(self,s1 = 1.0,s2 = 1.0 ,s3 = 1.0,color ="black",fll = False):
+        self.side1 = s1
+        self.side2 = s2
+        self.side3 = s3
+        self.color = color
+        self.fill = fll
+    def gets1(self):
+        return self.side1
+    def sets1(self,s1):
+        self.side1 = s1
+    def gets2(self):
+        return self.side2
+    def sets2(self,s2):
+        self.side2 = s2
+    def gets3(self):
+        return self.side3
+    def sets3(self,s3):
+        self.side3 = s3
+    def getArea(self):
+        s = (self.side1+self.side2+self.side3)/2
+        return (s*(s-self.side1)*(s-self.side2)*(s-self.side3))**0.5
+    def getPerimeter(self):
+        return self.side1 + self.side2+self.side3
+    def __str__(self):
+        return "Triangle: side1 =" + str(self.side1) + "side2 = " + str(self.side2) + "side3 = "+ str(self.side3)
+
+a,b,c = eval(input("변3개 입력 ,로 구분:"))
+color = input("색상 입력:")
+fll = eval(input("채웠는지 아닌지 구분(0,1)"))
+tri = Triangle(a,b,c,color,fll)
+print("삼각형의 넓이:{0} 둘레:{1} 색상:{2} 채워져있나?:{3}".format(tri.getArea(),tri.getPerimeter(),tri.color,tri.fill))
+
+#6번문제 13.1
+filename = input("파일이름 입력:")
+delstr = input("제거할 문자열 입력:")
+f = open(filename)
+line = f.read()
+line = line.replace(delstr,'')
+f.close()
+f = open(filename,'w')
+f.write(line)
+f.close()
+
+#7번문제 13.2
+filename = input("파일이름 입력:")
+f = open(filename)
+line = f.read()
+f.close()
+flist = line.split()
+fhang = line.split('\n')
+print("문자:",len(line))
+print("단어",len(flist))
+print("행:",len(fhang))
--- 3-1스언어과제/test.py
+++ 3-1스언어과제/test.py
@@ -1,17 +1,10 @@
-n = eval(input("2진수로 변화하고 싶은 10진수 입력: "))
-strg = []
-def decimalToBinary(value):
-    global strg
-    strg.append(value%2)
-    if value == 1:
-        return value
-    else:
-        value = value//2
-        return decimalToBinary(value)
-
-decimalToBinary(n)
-strg.reverse()
-res =""
-for i in strg:
-    res += str(i)
-print(res)
+#6번문제 13.1
+filename = "test.txt"
+f = open(filename)
+line = f.read()
+f.close()
+flist = line.split()
+fhang = line.split('\n')
+print("단어",len(flist))
+print("문자:",len(line))
+print("행:",len(fhang))
