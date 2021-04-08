64149c58c4d48d75e37450d87c7dd9b0b5fdd750
Spongebang <boy0501@naver.com>
2021-04-08 14:18:07 +0900

    modify

--- 3-1스언어과제/test.py
+++ 3-1스언어과제/test.py
@@ -1,10 +1,17 @@
-#6번문제 13.1
-filename = "test.txt"
-f = open(filename)
-line = f.read()
-f.close()
-flist = line.split()
-fhang = line.split('\n')
-print("단어",len(flist))
-print("문자:",len(line))
-print("행:",len(fhang))
+from tkinter import*
+def change_img():
+    path = inputBox.get()
+    img = PhotoImage(file = path+".gif")
+    imageLabel.configure(image = img)
+    imageLabel.image = img
+window = Tk()
+photo = PhotoImage(file="우주소녀.gif")
+imageLabel = Label(window,image=photo)
+imageLabel.pack()
+inputBox = Entry(window)
+inputBox.pack()
+button = Button(window,text='클릭',command=change_img)
+button.pack()
+
+
+window.mainloop()
